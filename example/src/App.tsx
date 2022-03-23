4ea60384a9707ef2e247cc3aed9120ec80450cc6
Kyaw Thura <ggaryal@gmail.com>
2022-03-23 21:56:56 +0630

    fix: added more example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,23 @@
 import * as React from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { Skeleton } from 'react-native-skeletons';
+import { Skeleton } from '../../src';
 
 export default function App() {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Skeleton
-          width={'100%'}
-          height={14}
-          count={4}
-          containerStyle={styles.mb10}
-        />
+        <Skeleton style={styles.mb10} />
         <Skeleton circle width={50} height={50} />
         <View style={styles.row}>
           <Skeleton circle width={50} height={50} />
           <Skeleton
-            width={'100%'}
-            height={14}
             count={2}
             containerStyle={styles.ml14}
+            spacing={10}
+            color={'grey'}
           />
         </View>
+        <Skeleton count={14} spacing={20} />
       </View>
     </SafeAreaView>
   );
@@ -32,12 +28,13 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginHorizontal: 20,
     flexGrow: 1,
-    // position: 'relative',
   },
   row: {
     flexDirection: 'row',
     marginTop: 10,
+    marginBottom: 20,
     alignItems: 'center',
+    overflow: 'hidden',
   },
   ml14: {
     marginLeft: 14,
